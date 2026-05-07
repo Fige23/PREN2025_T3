76c659293b16091a34798c8f7e53e38d2e806738
Felix Kaufmann <felix.r.kaufmann@outlook.com>
2026-05-07 15:25:10 +0200

    added "real world" config macros for pick and place

--- source/config/build_config.h
+++ source/config/build_config.h
@@ -51,7 +51,7 @@ build_config.h	Created on: 24.03.2026	   Author: Fige23	Team 3
 // Position feedback / closed loop
 #define POSITION_ENABLE                 1   // 0=no encoder, 1=encoder active
 #define POSITION_CLOSED_LOOP_ENABLE     1   // 0=measure only, 1=MOVE corrects position
-#define MOTION_TUNING_ENABLE            0   // 0=normal profiles, 1=UART TUNE command active
+#define MOTION_TUNING_ENABLE            1   // 0=normal profiles, 1=UART TUNE command active
 
 // -------------------------------------------------------------------------
 // DEBUG/TEST FEATURES (for development/diagnostics)
