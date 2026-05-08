434f2200b127be07e853fac7835f67f2350376e9
Felix Kaufmann <felix.r.kaufmann@outlook.com>
2026-05-08 15:58:22 +0200

    config changes

--- source/config/magnet_config.h
+++ source/config/magnet_config.h
@@ -3,8 +3,8 @@
 
 #define MAGNET_CHECK_ENABLE                     1
 
-#define MAGNET_PWM_FREQUENCY_HZ             1000u
-#define MAGNET_PWM_DEFAULT_DUTY_PERCENT       10u
+#define MAGNET_PWM_FREQUENCY_HZ             2000u
+#define MAGNET_PWM_DEFAULT_DUTY_PERCENT       50u
 #define MAGNET_PWM_PERIOD_TICKS              (STEP_TICK_HZ / MAGNET_PWM_FREQUENCY_HZ)
 
 #if MAGNET_PWM_PERIOD_TICKS < 2u
